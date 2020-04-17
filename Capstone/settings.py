065cc36acc8f2a5809882a083b4619593f86323d
sunga201 <sung9255@gmail.com>
2020-04-17 16:57:03 +0900

    수정 완료.

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -136,9 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+f=open('googleAccount.txt', 'r')
+account_info=f.read().split()
 EMAIL_HOST = 'smtp.gmail.com' # 메일을 호스트하는 서버
 EMAIL_PORT = '587' # gmail과 통신하는 포트
-EMAIL_HOST_USER = 'capstone8651@gmail.com' # 발신할 이메일
-EMAIL_HOST_PASSWORD = 'sungs201' # 발신할 메일의 비밀번호
+EMAIL_HOST_USER = account_info[0] # 발신할 이메일
+EMAIL_HOST_PASSWORD = account_info[1] # 발신할 메일의 비밀번호
 EMAIL_USE_TLS = True # TLS 보안 방법
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER # 사이트와 관련된 자동응답을 받을 이메일 주소, 'webmaster@localhost'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER # 사이트와 관련된 자동응답을 받을 이메일 주소
