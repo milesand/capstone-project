e84c249a3db03373f960baa70de708e064124051
Jewoo Lee <shema7k@gmail.com>
2020-05-02 17:08:27 +0900

    fix mentions of Capstone and Capstone.config

--- app/capstone/wsgi.py
+++ app/capstone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Capstone.config.deploy')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.config.deploy')
 
 application = get_wsgi_application()
