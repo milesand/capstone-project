96b57c73c93b160b60c78a701a948fb6ee6c693a
sunga201 <sungs201@naver.com>
2020-05-05 17:56:09 +0900

    PR 테스트 2.

--- React/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import auth from './auth'; // added
-
-export default combineReducers({
-  form: formReducer,
-  todos,
-  auth // added
-});
--- app/capstone/account/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    name = 'capstone.account'
-    label = 'capstone_account'
