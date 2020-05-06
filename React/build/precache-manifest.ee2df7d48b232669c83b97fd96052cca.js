45222b4067ecfe3636ca30995baef8bb28762e8b
sunga201 <sungs201@naver.com>
2020-05-06 16:56:20 +0900

    빌드파일 수정.

---
+++ React/build/precache-manifest.ee2df7d48b232669c83b97fd96052cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71128caaea43e34ce859",
+    "url": "./static/css/main.c5858ca3.chunk.css"
+  },
+  {
+    "revision": "71128caaea43e34ce859",
+    "url": "./static/js/main.71128caa.chunk.js"
+  },
+  {
+    "revision": "b9b0f4f81a375712dc61",
+    "url": "./static/js/1.b9b0f4f8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fd59186f07aaee484e3ddb7ac2be2676",
+    "url": "./static/media/mail.fd59186f.png"
+  },
+  {
+    "revision": "bfde4c7629ebfec8e4a7235d7c1c8f6d",
+    "url": "./index.html"
+  }
+];
