aa9ebf05210f4b748842510c0f986f316968acfd
Jewoo Lee <shema7k@gmail.com>
2020-05-14 16:09:09 +0900

    move url spec into site urls.py, outside app

--- app/capstone/apps/storage/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FlowUploadStartView, FlowUploadChunkView
-
-urlpatterns = [
-    path('flow', FlowUploadStartView.as_view()),
-    path('flow/<str:id>', FlowUploadChunkView.as_view()),
-]
