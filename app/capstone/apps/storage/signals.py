9cfc38cb7f7dff5033304e8062c2505d6cec4345
Jewoo Lee <shema7k@gmail.com>
2020-05-24 14:16:05 +0900

    add user creation hook
    Now UserStorage and root directory is created at user creation,
    not at first upload. This allows us to assume they 'just exist', and
    limit DB locking to that particular user's row. Also removed code
    that tried to create UserStorage object if it doesn't exist, because
    now if it doesn't exist it's an hard error.

--- app/capstone/apps/storage/signals.py
+++ app/capstone/apps/storage/signals.py
@@ -1,12 +1,14 @@
 from django.db import transaction
-from django.db.models.signals import post_delete, pre_delete
+from django.db.models.signals import post_delete, pre_delete, post_save
 from django.conf import settings
 from django.dispatch import receiver
 
-from .models import UserStorage, PartialUpload, File
+from .models import UserStorage, PartialUpload, Directory, File
 from pathlib import Path
 
 import os
+
+
 @receiver(post_delete, sender=File)
 def delete_file(sender, instance, using, **kwargs):
     print('instance : ', instance)
@@ -48,6 +50,7 @@ def delete_file(sender, instance, using, **kwargs):
 
     print('file_name : ', file_name, ', path : ', path)
 
+
 @receiver(post_delete, sender=PartialUpload)
 def delete_partial_upload(sender, instance, using, **kwargs):
     print('instance : ', instance)
@@ -61,9 +64,24 @@ def delete_partial_upload(sender, instance, using, **kwargs):
             return
         # Bump up the user's storage capacity.
         with transaction.atomic():
-            # If partial storage exists, then its uploader must have requested an storage,
-            # which would have created the UserStorage object for the user.
-            # Thus, the UserStorage object is sure to exist.
-            storage = UserStorage.objects.get(user=instance.uploader)
+            storage = UserStorage.objects.filter(user=instance.uploader).select_for_update().get()
             storage.remove(instance.file_size)
             storage.save()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def setup_user(_sender, instance, created, using, _update_fields):
+    '''Set up UserStorage and root directory for the user.'''
+
+    if not created:
+        return
+
+    root_dir = Directory.objects.using(using).create(
+        owner=instance,
+        name="",
+        parent=None,
+    )
+    UserStorage.objects.using(using).create(
+        user=instance,
+        root_dir=root_dir,
+    )
