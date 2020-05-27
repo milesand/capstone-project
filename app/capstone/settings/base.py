9f9f7be28621e1f57d2b6c829a6f10ca5da9f2c8
Jewoo Lee <shema7k@gmail.com>
2020-05-27 11:57:23 +0900

    unify timezone to UTC

--- app/capstone/settings/base.py
+++ app/capstone/settings/base.py
@@ -214,13 +214,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Asia/Seoul'
-
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
