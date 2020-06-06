3d5455b073b696300c1fba650eb777f1a73fae9a
Jewoo Lee <shema7k@gmail.com>
2020-06-06 13:21:06 +0900

    separate dev and prod environment

--- app/capstone/settings/develop.py
+++ app/capstone/settings/develop.py
@@ -1,16 +1,4 @@
 from .base import *
 
-DEBUG=True
-
-CORS_ALLOW_CREDENTIALS = True
-
-DATABASES = {
-    'default': {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'capstonedb',
-        'USER' : 'cs',
-        'PASSWORD' : '1234',
-        'HOST' : 'localhost', #이부분에 서버 IP 넣기
-        'PORT' : '5432',
-    },
-}
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
