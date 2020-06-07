05dfab48ced7855f26e173407732ca560f94d8f9
Jewoo Lee <shema7k@gmail.com>
2020-06-07 11:32:04 +0900

    disable staging flag
    Everything seems to be working, so disabled staging.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -33,7 +33,7 @@ domains=(moongedrive.com www.moongedrive.com)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email=""
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
