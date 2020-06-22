184deb1eef974b967401e5da458d20a15192879c
sungs201 <49271247+sunga201@users.noreply.github.com>
2020-06-22 14:40:40 +0900

    Update urls.py
    URL 중복 및 잘못된 주석 설명 수정.

--- app/capstone/urls.py
+++ app/capstone/urls.py
@@ -85,9 +85,8 @@ api = [
     path('join-team', teams.JoinTeamAPI.as_view()), # 현재 가입중인 팀 목록 출력
     path('search-user/<str:team_pk>/<str:name>', teams.UserSearchAPI.as_view()), # 일부 문자열을 통해 사용자 검색
 
-    path('partial', storage.PartialAPI.as_view()), #partial file 목록 출력 및 전체 제거, 테스트용
     re_path('partial/(?P<pk>[0-9a-z-]{36})', storage.PartialDeleteAPI.as_view()),  # 특정 partial file 제거, 업로드 중단할 때 사용
-    re_path('streaming/(?P<pk>[0-9a-z-]{36})', storage.StreamingAPI.as_view()),  # 특정 partial file 제거, 업로드 중단할 때 사용
+    re_path('streaming/(?P<pk>[0-9a-z-]{36})', storage.StreamingAPI.as_view()),  # 이미지 및 동영상 미리보기를 위한 파일 경로 제공
 
     path('favorite', storage.FavoriteView.as_view()),
     path('recycle', storage.RecycleBinView.as_view()),
